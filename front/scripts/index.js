7635ddd0f3e775e5cc5bbd8dc8483736c8eff55c
diazkr <juliethdiaz001@gmail.com>
2024-03-05 17:11:04 -0500

    feat():cambiar a innerHTML y luego usar la api de movies

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,60 +1,59 @@
-console.log(tempData);
 
+
+const wrapperContainer = document.getElementById("wrapper");
 
 const crearCard = function(objeto){
 
     //Crear la card y luego la añadimos al wrapper container
     var card = document.createElement("div");
     card.classList.add("swiper-slide", "card");
-    wrapperContainer.appendChild(card);
-
-    // Creamos un div donde estara el contetnido de la card
-    var content =  document.createElement("div");
-    content.classList.add("card-content");
-    card.appendChild(content)
-
-    //Creacion del div de la imagen
-    var divImage = document.createElement('div')
-    divImage.classList.add("image")
-    var imagenElement = document.createElement('img')
-    imagenElement.src = objeto.poster;
-    imagenElement.alt ="";
-    divImage.appendChild(imagenElement)
-
-    //creamos el div del texto name y añadimos el contenido
-    var divText = document.createElement('div')
-    divText.classList.add("name-profession")
-    var nameElement = document.createElement('span');
-    nameElement.classList.add("name")
-    nameElement.textContent = objeto.title;
-
-    //realizamos lo mismo pero con profesion
-    var professionElement = document.createElement('span');
-    professionElement.classList.add("profession");
-    professionElement.textContent = objeto.duration;
-
-    //aññadimos los elementos
-    divText.appendChild(nameElement);
-    divText.appendChild(professionElement);
 
 
-    //creamos el div donde va el boton de ver
-    var divBotonVer = document.createElement('div')
-    divBotonVer.classList.add('button')
-    var boton = document.createElement("button")
-    boton.classList.add("aboutMe")
-    boton.textContent = "Ver más";
-    divBotonVer.appendChild(boton);
+    card.innerHTML = `
+            <div class="card-content">
+                <div class="image">
+                    <img src="${objeto.poster}" alt="">
+                </div>
 
-    //añadimos los 3 elementos al card container
-    content.appendChild(divImage);
-    content.appendChild(divText);
-    content.appendChild(divBotonVer);
+                <div class="name-profession">
+                    <span class="name">${objeto.title}</span>
+                    <span class="profession"> ${objeto.duration}</span>
+                </div>
+
+                <div class="button">
+                    <button class="aboutMe">Ver más</button>
+                </div>
+            </div>
+        `
+        
+        wrapperContainer.appendChild(card);
     
 }
-const wrapperContainer = document.getElementById("wrapper");
 
-const ejecutarCards = tempData.map(pelicula => crearCard(pelicula));
+
+//const ejecutarCards = tempData.map(pelicula => crearCard(pelicula));
+
+
+//Creamos la función que va a iterar sobre los elementos de la lista, que son objetos
+const funcionIteradora = function(list){
+
+    list.forEach(objetoMovie =>crearCard(objetoMovie));
+    list.forEach(objetoMovie =>crearCard(objetoMovie)); //**LUEGO BORRAR */
+    list.forEach(objetoMovie =>crearCard(objetoMovie)); //**LUEGO BORRAR */
+}
+
+
+// acá hacemos el get del array de objetos y luego llamamos a la función que itera
+const getObject = function(){
+    $.get('https://students-api.2.us-1.fl0.io/movies', array => funcionIteradora(array));
+}
+
+
+getObject();
+
+
+
+
 
 
 
@@ -62,8 +61,7 @@ const ejecutarCards = tempData.map(pelicula => crearCard(pelicula));
 var swiper = new Swiper(".mySwiper", {
     
     spaceBetween: 30,
-    loop: true,
-    loopFillGroupWithBlank: true,
+    loop: false, //true
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
@@ -72,7 +70,7 @@ var swiper = new Swiper(".mySwiper", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
-    centeredSlides: true, // Centra las diapositivas
+    //centeredSlides: true, // Centra las diapositivas
     breakpoints: {
         // Configuración cuando el ancho de la pantalla es igual o mayor a 768px
         768: {
