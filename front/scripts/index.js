f4cbaaa6eecc7b309f1020adf5a5c797651bc384
diazkr <juliethdiaz001@gmail.com>
2024-03-04 22:04:16 -0500

    feat(static page)

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,91 @@
 console.log(tempData);
+
+
+const crearCard = function(objeto){
+
+    //Crear la card y luego la añadimos al wrapper container
+    var card = document.createElement("div");
+    card.classList.add("swiper-slide", "card");
+    wrapperContainer.appendChild(card);
+
+    // Creamos un div donde estara el contetnido de la card
+    var content =  document.createElement("div");
+    content.classList.add("card-content");
+    card.appendChild(content)
+
+    //Creacion del div de la imagen
+    var divImage = document.createElement('div')
+    divImage.classList.add("image")
+    var imagenElement = document.createElement('img')
+    imagenElement.src = objeto.poster;
+    imagenElement.alt ="";
+    divImage.appendChild(imagenElement)
+
+    //creamos el div del texto name y añadimos el contenido
+    var divText = document.createElement('div')
+    divText.classList.add("name-profession")
+    var nameElement = document.createElement('span');
+    nameElement.classList.add("name")
+    nameElement.textContent = objeto.title;
+
+    //realizamos lo mismo pero con profesion
+    var professionElement = document.createElement('span');
+    professionElement.classList.add("profession");
+    professionElement.textContent = objeto.duration;
+
+    //aññadimos los elementos
+    divText.appendChild(nameElement);
+    divText.appendChild(professionElement);
+
+
+    //creamos el div donde va el boton de ver
+    var divBotonVer = document.createElement('div')
+    divBotonVer.classList.add('button')
+    var boton = document.createElement("button")
+    boton.classList.add("aboutMe")
+    boton.textContent = "Ver más";
+    divBotonVer.appendChild(boton);
+
+    //añadimos los 3 elementos al card container
+    content.appendChild(divImage);
+    content.appendChild(divText);
+    content.appendChild(divBotonVer);
+    
+}
+const wrapperContainer = document.getElementById("wrapper");
+
+const ejecutarCards = tempData.map(pelicula => crearCard(pelicula));
+
+
+
+
+var swiper = new Swiper(".mySwiper", {
+    
+    spaceBetween: 30,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    centeredSlides: true, // Centra las diapositivas
+    breakpoints: {
+        // Configuración cuando el ancho de la pantalla es igual o mayor a 768px
+        768: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+            slidesPerGroup: 1,
+        },
+        // Configuración cuando el ancho de la pantalla es igual o mayor a 1024px
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+            slidesPerGroup: 1,
+        },
+    },
+});
+
--- front/scripts/tempData.js
+++ front/scripts/tempData.js
@@ -17,7 +17,7 @@ const tempData = [
     genre: ["Action", "Adventure", "Fantasy", "Sci-Fi"],
     rate: 8.7,
     poster:
-      "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg",
+      "https://i.pinimg.com/564x/f0/eb/d6/f0ebd6d772eaa8221f7b9ab82f4e41a5.jpg",
   },
   {
     title: "The Lord of the Rings: The Fellowship of the Ring",
@@ -29,4 +29,49 @@ const tempData = [
     poster:
       "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
   },
+  {
+    title: "Beauty and the Beast",
+    year: 1991,
+    director: "Gary Trousdale, Kirk Wise",
+    duration: "1h 24min",
+    genre: ["Animation", "Family", "Fantasy", "Musical", "Romance"],
+    rate: 8.0,
+    poster: "https://i.pinimg.com/564x/0c/8e/51/0c8e51067e8879f60ff844de33e9ad21.jpg"
+  },
+  {
+    title: "Up",
+    year: 2009,
+    director: "Pete Docter, Bob Peterson",
+    duration: "1h 36min",
+    genre: ["Animation", "Adventure", "Comedy", "Family"],
+    rate: 8.2,
+    poster: "https://i.pinimg.com/564x/cd/ac/25/cdac25907202528f6cc79c22aaf4fd44.jpg"
+  },
+  {
+    title: "Cinderella",
+    year: 1950,
+    director: "Clyde Geronimi, Wilfred Jackson",
+    duration: "1h 14min",
+    genre: ["Animation", "Family", "Fantasy", "Musical", "Romance"],
+    rate: 7.3,
+    poster: "https://i.pinimg.com/564x/b0/16/60/b0166064857c210f7ddebfbbbebb0d7b.jpg"
+  },
+  {
+    title: "La La Land",
+    year: 2016,
+    director: "Damien Chazelle",
+    duration: "2h 8min",
+    genre: ["Comedy", "Drama", "Music", "Musical", "Romance"],
+    rate: 8.0,
+    poster: "https://i.pinimg.com/564x/1d/c6/5b/1dc65b5a934d1b44683f0abf788a6f3c.jpg"
+  },
+  {
+    title: "Eternal Sunshine of the Spotless Mind",
+    year: 2004,
+    director: "Michel Gondry",
+    duration: "1h 48min",
+    genre: ["Drama", "Romance", "Sci-Fi"],
+    rate: 8.3,
+    poster: "https://i.pinimg.com/564x/1c/09/5c/1c095c53803ec74861d11df51e05722b.jpg"
+  }
 ];
