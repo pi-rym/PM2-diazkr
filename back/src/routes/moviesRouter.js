926e481210ee8dcbcb2c7fd5f839c7af776c220b
kdiazc1 <kdiazc@unal.edu.co>
2024-03-16 17:36:39 -0500

    feat(): deleat class Movies

--- back/src/routes/moviesRouter.js
+++ back/src/routes/moviesRouter.js
@@ -1,5 +1,4 @@
 const {Router} = require('express');
-//const {testMovies, createMovie} = require('../controllers/moviesController')
 
 const testMovies = require('../controllers/moviesController')
 const validateProduct = require("../middleware/movieMiddleware")
@@ -8,8 +7,6 @@ const moviesRouter = Router();
 
 
 moviesRouter.get("/", testMovies)
-
-//moviesRouter.post("/",validateProduct, createMovie)
 
 
 module.exports = moviesRouter;
--- back/src/services/moviesServices.js
+++ back/src/services/moviesServices.js
@@ -1,50 +1,6 @@
 
 const axios = require('axios');
 
-
-//Acá creamos la clase movie y podemos añador pelis
-
-/*
-class Movie {
-    constructor(title,date, genere,duration,poster){
-        this.title = title;
-        this.date = date;
-        this.genere = genere;
-        this.duration = duration;
-        this.poster = poster;
-    }
-}
-
-class Repository{
-
-    constructor(){
-        this.lista = [];
-    }
-
-    createMovie({title,date, genere,duration,poster}){
-        const nuevaMovie = new Movie(title,date,genere,duration,poster);
-        
-        this.lista.push(nuevaMovie);
-    }
-
-    traerMovie(){
-        return this.lista;
-    }
-
-    
-}
-
-module.exports = {
-    Repository,
-    Movie
-};
-
-*/
-
-
-
-
-
 const traerMovies = async () =>{
 
     try {
